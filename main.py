f3bbeeba6ad72622bfce661772a7143120c9976f
Nordmann92 <nordmann92@t-online.de>
2024-05-09 22:32:27 +0200

    player chose name and move

--- main.py
+++ main.py
@@ -17,8 +17,25 @@ class Character:
 def welcome():
     print('Willkommen zu unserem Spiel "Schere, Stein, Papier"')
 
+def player_chose_name():
+    a = input("Bitte gib deinen Spielernamen an: ")
+    return a
+
+
+
+def player_chose_move():
+    a = input("Bitte wähle deinen Spielzug! Schere (1), Stein (2) oder Papier (3): ")
+    a = a.lower()
+    if a == "1" or a == "schere":
+        return "scissor"
+    elif a == "2" or a == "stein":
+        return "rock"
+    elif a == "3" or a == "papier":
+        return "paper"
+    else:
+        print("Deine Eingabe war falsch!")
+        return player_chose_move()
+
 
 if __name__ == '__main__':
-    p1 = Character('Test')
-    print(p1.name)
-    print(p1.score)
+    print(player_chose_move())
