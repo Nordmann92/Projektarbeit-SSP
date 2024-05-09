16833b22c831ee66768c47ca9e462c7b20663c0e
Nordmann92 <nordmann92@t-online.de>
2024-05-09 17:32:57 +0200

    class

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 # main File
 
+class Character:
+    def __init__(self, name, score, win, lose, move):
+        self.name = name
+        self.score = score
+        self.win = win
+        self.lose = lose
+        self.move = move
 
-print('Willkommen zu unserem Spiel "Schere, Stein, Papier"')
+    def show_score(self):
+        return self.score
+
+    def show_move(self):
+        return self.move
+
+def welcome():
+    print('Willkommen zu unserem Spiel "Schere, Stein, Papier"')
 
