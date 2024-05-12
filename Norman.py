2931600a6a6617bfac7f7620c1cbfb68ae3bf17a
Nordmann92 <nordmann92@t-online.de>
2024-05-13 00:09:47 +0200

    Choose winner!

--- Norman.py
+++ Norman.py
@@ -1,2 +1,24 @@
-def choose_winner():
-    pass
+def choose_winner(player_a, player_b) -> object:
+
+    if player_a.move == player_b.move:
+        return "Unentschieden"
+    if ((player_a.move == "rock" and player_b.move == "scissor") or
+            (player_a.move == "paper" and player_b.move == "rock") or
+            (player_a.move == "scissor" and player_b.move == "paper")):
+        player_a.score += 1
+        player_a.win += 1
+        player_b.score -= 1
+        player_b.lose += 1
+        return player_a.name
+    if ((player_a.move == "rock" and player_b.move == "paper") or
+            (player_a.move == "paper" and player_b.move == "scissor") or
+            (player_a.move == "scissor" and player_b.move == "rock")):
+        player_a.score -= 1
+        player_a.lose += 1
+        player_b.score += 1
+        player_b.win += 1
+        return player_b.name
+    else:
+        return "fehler"
+
+
